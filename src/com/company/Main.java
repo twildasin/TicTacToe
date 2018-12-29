b8f6532b6a666485dddb65add5036bcfa79837d6
twildasin <tjwild127@aol.com>
2018-12-29 00:15:32 -0500

    Final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int moves = 0;
         char grid [] [] = new char [3] [3];
         Scanner kbin = new Scanner(System.in);
         String in;
@@ -16,14 +17,14 @@ public class Main {
 
         //clear the grid
         for(int i = 0; i<3; i++)
-        {
-            grid [0] [i] = ' ';
-            grid [1] [i] = ' ';
-            grid [2] [i] = ' ';
+        { //made by tanner
+            for (int k = 0; k < 3; k++) {
+                grid[k][i] = ' ';
+            }
         }
 
         System.out.println("Welcome to tic tac toe!\nInstructions:\n1: You are \"X's\", the computer is \"O's\"\n2: chose a position to place a marker by giving two coordinates");
-        System.out.println("\nExamples, inputting \"01\" will place a marker to look like this:");
+        System.out.println("\nExamples, inputting \"23\" will place a marker to look like this:");
         grid [0] [1] = 'X';
         function.printgird(grid);
         grid [0] [1] = ' ';
@@ -58,24 +59,72 @@ public class Main {
         {
             grid[0][0] = 'O';
             function.printgird(grid);
+            moves++;
         }
 
-        System.out.println("Please enter to coordinate to play:");
-        while(true)
+
+
+        function.askForMove(grid);
+        moves++;
+
+        if(playerfirst && Objects.equals(grid [1] [1] , ' '))
         {
-            in = kbin.nextLine();
-            if (function.markplayer(grid, in) == true)
+            grid [1] [1] = 'O';
+            moves++;
+            function.printgird(grid);
+        }
+
+
+
+
+        boolean tie;
+        for(int i = moves; i < 9; i++)
+        {
+            if((playerfirst && moves % 2 == 0) || (!playerfirst && moves %  2 != 0))
             {
+                function.askForMove(grid);
+                moves++;
+            }
+            else
+            {
+                function.findMove(grid);
+                moves++;
+                function.printgird(grid);
+
+            }
+            if(function.checkWin(grid) == 0)
+            {
+                tie =false;
+                System.out.println("Computer Won!");
                 break;
             }
-            else {
-                System.out.println("Invalid Input, please enter a valid coordinate that is not in use (Ex: \"01\")");
+        }
+        System.out.println("Tie!");
+
+
+
+
+
+
+
+        /*for(int i = 0; i<3; i++)
+        {
+            for (int k = 0; k < 3; k++) {
+                grid[k][i] = ' ';
             }
         }
 
-
-        //int r = Integer.parseInt("p");
-
+        grid [1] [0] = 'O';
+        grid [1] [1] = ' ';
+        grid [2] [2] = ' ';
+        grid [0] [0] = 'X';
+        grid [0] [1] = ' ';
+        grid [1] [2] = 'X';
+        function.printgird(grid);
+        //System.out.println(function.checkWin(grid));
+        function.findMove(grid);
+        //System.out.println(function.findStop(grid, ' ', 'O', 2));
+        function.printgird(grid);*/
 
 
 
--- src/com/company/function.java
+++ src/com/company/function.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.util.Objects;
+//wildasin,t
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by tw073 on 12/17/18.
@@ -35,7 +37,7 @@ public class function {
     public static boolean markplayer (char [] [] a, String b)
     {
         b = b.replaceAll(" ", "");
-        if(function.checkInput(b)) {
+        if(function.checkInput(b)) { //TJW as here
             int col = Integer.parseInt(b.substring(0, 1));
             int row = Integer.parseInt(b.substring(1, 2));
             if (Objects.equals(a[col][row], ' '))
@@ -103,11 +105,274 @@ public class function {
 
     }
 
-    public static boolean checkWin(char [] [] a)
+    public static int checkWin(char [] [] a)
     {
-        boolean Xwin;
-        for()
+        int Xwin = -1;
+        for(int i = 0; i < 3; i++)
+        {
+            Xwin = ((Objects.equals(a [i] [0], 'X') && (Objects.equals(a [i] [1], 'X')) && (Objects.equals(a [i] [2], 'X'))) ? 1 : Xwin);
+            Xwin = ((Objects.equals(a [0] [i], 'X') && (Objects.equals(a [1] [i], 'X')) && (Objects.equals(a [2] [i], 'X'))) ? 1 : Xwin);
+
+            Xwin = ((Objects.equals(a [i] [0], 'O') && (Objects.equals(a [i] [1], 'O')) && (Objects.equals(a [i] [2], 'O'))) ? 0 : Xwin);
+            Xwin = ((Objects.equals(a [0] [i], 'O') && (Objects.equals(a [1] [i], 'O')) && (Objects.equals(a [2] [i], 'O'))) ? 0 : Xwin);
+        }
+
+        Xwin = ((Objects.equals(a [0] [0], 'X') && (Objects.equals(a [1] [1], 'X')) && (Objects.equals(a [2] [2], 'X'))) ? 1 : Xwin);
+        Xwin = ((Objects.equals(a [0] [2], 'X') && (Objects.equals(a [1] [1], 'X')) && (Objects.equals(a [2] [0], 'X'))) ? 1 : Xwin);
+
+        Xwin = ((Objects.equals(a [0] [0], 'O') && (Objects.equals(a [1] [1], 'O')) && (Objects.equals(a [2] [2], 'O'))) ? 0 : Xwin);
+        Xwin = ((Objects.equals(a [0] [2], 'O') && (Objects.equals(a [1] [1], 'O')) && (Objects.equals(a [2] [0], 'O'))) ? 0 : Xwin);
+    return Xwin;
     }
+
+    public static void findMove (char [] [] a)
+    {
+        String compWin = function.findStop(a,'O',' ', 1);
+        String playerWin = function.findStop(a,'X',' ', 1);
+        String increase = function.findStop(a, ' ', 'O', 2);
+        if(Objects.equals(compWin, "-1"))
+        {
+            if(Objects.equals(playerWin, "-1"))
+            {
+                if(Objects.equals(increase, "-1"))
+                {
+                    function.playRandom(a);
+                }
+                else // If system detects move to branch off of its own marker
+                {
+                    a [Integer.parseInt(increase.substring(0,1))] [Integer.parseInt(increase.substring(1,2))] = 'O';
+                }
+            }
+            else // If  system found potential win for player
+            {
+                a [Integer.parseInt(playerWin.substring(0,1))] [Integer.parseInt(playerWin.substring(1,2))] = 'O';
+            }
+        }
+        else // If system found potential win for computer (Made by tanner)
+        {
+            a [Integer.parseInt(compWin.substring(0,1))] [Integer.parseInt(compWin.substring(1,2))] = 'O';
+        }
+
+    }
+
+    public static void askForMove (char [] [] a)
+    {
+        String in = "";
+        Scanner kbin = new Scanner(System.in);
+        System.out.println("Please enter to coordinate to play:");
+        while(true)
+        {
+            in = kbin.nextLine();
+            in = (Objects.equals(in, "11") ? "20" : in);
+            in = (Objects.equals(in, "21") ? "21" : in);
+            in = (Objects.equals(in, "31") ? "22" : in);
+            in = (Objects.equals(in, "12") ? "10" : in);
+            in = (Objects.equals(in, "22") ? "11" : in);
+            in = (Objects.equals(in, "32") ? "12" : in);
+            in = (Objects.equals(in, "13") ? "00" : in);
+            in = (Objects.equals(in, "23") ? "01" : in);
+            in = (Objects.equals(in, "33") ? "02" : in);
+
+            if (function.markplayer(a, in) == true)
+            {
+                break;
+            }
+            else {
+                System.out.println("Invalid Input, please enter a valid coordinate that is not in use (Ex: \"01\")");
+            }
+        }
+    }
+
+    public static String findStop (char [] [] a, char primary, char secondary, int choice)
+    {
+        boolean end = false;
+        boolean endy = false;
+        //int posb1 = 3;
+        //by tanner
+        //int posb2 = 3;
+        int col = -1;
+        int row = -1;
+        int d = -1;
+        int d2 = -1;
+        int pos1 = 3;
+        int pos2 = 3;
+        int pos1y = 3;
+        int pos2y = 3;
+        int countx = 0;
+        int count_ = 0;
+        int county = 0;
+        int county_ = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+
+                if(Objects.equals(a [i] [k], secondary))
+                {
+                    count_++;
+                    if(choice == 1)
+                    {
+                        pos1 = i;
+                        pos2 = k;
+                    }
+                }
+                if(Objects.equals(a [i] [k], primary))
+                {
+                    if (choice == 2)
+                    {
+                        pos1 = i;
+                        pos2 = k;
+                    }
+                    countx++;
+                }
+                if (countx == 2 && count_ == 1)
+                {
+                    col = i;
+                    end = true;
+                }
+
+                //-------------------------------------------------------
+
+
+                if(Objects.equals(a [k] [i], secondary))
+                {
+                    county_++;
+                    if(choice == 1)
+                    {
+                        pos1y = i;
+                        pos2y = k;
+                    }
+                }
+                if(Objects.equals(a [k] [i], primary))
+                {
+                    if (choice == 2)
+                    {
+                        pos1y = i;
+                        pos2y = k;
+                    }
+                    county++;
+                }
+                if (county == 2 && county_ == 1)
+                {
+                    row = i;
+                    endy = true;
+                }
+
+
+            }
+            countx = 0;
+            count_ = 0;
+            county = 0;
+            county_ = 0;
+            //by tanner
+            if (end || endy)
+                break;
+            pos1 = 3;
+            pos2 = 3;
+            pos1y = 3;
+            pos2y = 3;
+        }
+
+        //---------------------------------------------------------
+
+
+        boolean endd = false;
+        int pos1d = 0;
+        int pos2d = 0;
+        int countd = 0;
+        int count_d = 0;
+        for(int i = 0; i < 3; i++)
+        {
+
+            if(Objects.equals(a [i] [i], secondary))
+            {
+                count_d++;
+                if(choice == 1)
+                {
+                    pos1d = i;
+                    pos2d = i;
+                }
+            }
+            if(Objects.equals(a [i] [i], primary))
+            {
+                if(choice == 2)
+                {
+                    pos1d = i;
+                    pos2d = i;
+                }
+                countd++;
+            }
+            if (countd == 2 && count_d == 1)
+            {
+                d = 1;
+                endd = true;
+            }
+            else if (i == 2)
+            {
+                pos1d = 3;
+                pos2d = 3;
+            }
+        }
+
+        if (!endd)
+        {
+            count_d = 0;
+            countd = 0;
+            int inv = 2;
+
+
+            for(int i = 0; i < 3; i++)
+            {
+                if(Objects.equals(a [inv] [i], primary))
+                {
+                    if(choice == 2)
+                    {
+                        pos1d = inv;
+                        pos2d = i;
+                    }
+                    countd++;
+                }
+                if(Objects.equals(a [inv] [i], secondary))
+                {
+                    count_d++;
+                    if(choice == 1)
+                    {
+                        pos1d = inv;
+                        pos2d = i;
+                    }
+                }
+                if (countd == 2 && count_d == 1)
+                {
+                    d = 2;
+                    endd = true;
+                }
+                else if (i == 2)
+                {
+                    pos1d = 3;
+                    pos2d = 3;
+                }
+                inv--;
+            }
+
+
+        }
+
+        //---------------------------------------------------------
+        //System.out.println(pos1d + "" + pos2d);
+
+
+
+        if(end)
+            return (pos1 + "" + pos2);
+        else if (endy)
+            return (pos2y + "" + pos1y);
+        else if (endd)
+            return (pos1d + "" + pos2d);
+
+        else
+            return ("-1");
+    }
+
 
 
 }
